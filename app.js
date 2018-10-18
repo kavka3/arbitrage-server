d7064872b5d1e88673d5f3fe31f6811bcbf5f289
Oleg Shmuelov <oleg.shmuelov@gmail.com>
2018-10-19 01:24:49 +0300

    running example ogf nodejs bull

--- app.js
+++ app.js
@@ -25,53 +25,20 @@ app.use(errorHandler);
 
 function getRedisConfig(redisUrl) {
     const redisConfig = url.parse(redisUrl);
+    console.log(redisConfig);
     return {
-        host: redisConfig.hostname || 'localhost',
+        host: 'sadadasdad/asdasd',
         port: Number(redisConfig.port || 6379),
         database: (redisConfig.pathname || '/0').substr(1) || '0',
         password: redisConfig.auth ? redisConfig.auth.split(':')[1] : undefined
     };
 }
 
-prdsMonQueue = new Queue('test',
-    {
-        limiter: {
-            max: 1000, // not work even 1000
-            duration: 10
-        }
-    }
-)
-
-const monitoring = async (product) => {
-    console.log('done', product)
-    return product
-}
-prdsMonQueue.on('completed', async function (job, result) {
-    console.log('run', result)
-})
-const addJob = function (product) {
-    let uid = uuid.v1()
-    prdsMonQueue.process(uid, function (job) {
-        return monitoring(job.data)
-    })
-    prdsMonQueue.add(uid, product, {
-        repeat: {
-            cron: `*/5 * * * * *`
-        }
-    })
-}
-
-const products = ['A', 'B', 'C', 'D']
-for (let i = 0; i < products.length; i++) {
-    addJob(products[i])
-}
-
-
 app.use('/', Arena(
     {
         queues: [
             {
-                name: 'test',
+                name: 'test - queue',
                 hostId: 'Worker',
                 redis: getRedisConfig(process.env.REDIS_URL)
             }
@@ -83,6 +50,16 @@ app.use('/', Arena(
     }
 ));
 
+var testQueue = new Queue('test queue', 'redis://redis:6379');
+
+testQueue.process(function (job) {
+    // Check payments
+
+    console.log("RIKIBKAKA");
+
+});
+
+testQueue.add({}, { repeat: { cron: '15 3 * * *' } });
 
 var listener = app.listen(process.env.PORT || 3000, () => {
     console.log('Server started PORT', + listener.address().port);
